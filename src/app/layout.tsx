be48fc9129e9783da1011cb2e475e060439a720e
ariefgp <arief.gusti@gmail.com>
2024-10-02 09:46:35 +0700

    fix build issue in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,29 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import { AuthProvider } from './contexts/AuthContext'
-import GoogleAnalytics from '../components/GoogleAnalytics'
+import './globals.css';
+import { Inter } from 'next/font/google';
+import { AuthProvider } from './contexts/AuthContext';
+import GoogleAnalytics from '../components/GoogleAnalytics';
+import { Suspense } from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Pomofly',
   description: 'A Minimal Pomodoro timer with task and project management',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <AuthProvider>{children}</AuthProvider>
-        <GoogleAnalytics GA_MEASUREMENT_ID={process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!} />
+        <Suspense fallback={<div>Loading...</div>}>
+          <GoogleAnalytics GA_MEASUREMENT_ID={process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!} />
+        </Suspense>
       </body>
     </html>
-  )
+  );
 }
